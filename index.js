42ffa934b9e8607ecc8f060507e5ef6141228a4f
ahmed-shafique <ahmed.shafique94@gmail.com>
2022-06-01 11:07:10 +0500

    PortFolio Changes

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import sgMail from "@sendgrid/mail";
+import cors from "cors";
+import sendMail from "./utils/mailer/index.js";
+
+sgMail.setApiKey(
+  "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"
+);
+
+dotenv.config({ path: "./config.env" });
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.use(express.json());
+app.disable("x-powered-by");
+
+app.post("/send-mail", (req, res) => {
+  const { name, email, number, subject, text } = req.body;
+  console.log(name, email, number, subject, text);
+
+  const response = sendMail(name, email, number, subject, text);
+
+  console.log(response);
+
+  res.json({
+    message: "success",
+    ...req.body,
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+(async () => {
+  try {
+    app.listen({ port: PORT }, () =>
+      console.log(`🚀 Server ready at http://localhost:${PORT} 🚀`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+})();
---
+++ utils/mailer/index.js
@@ -0,0 +1,54 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import sgMail from "@sendgrid/mail";
+dotenv.config();
+//TODO:// check if env variables are set
+async function sendMail(name, email, number, subject, text) {
+  try {
+    let transporter = nodemailer.createTransport({
+      //host: "smtp.ionos.com",
+      service: "Gmail",
+      auth: {
+        user: "ahmed.shafique94@gmail.com",
+        pass: "vgxipmugsrxgmmnr",
+      },
+    });
+    let info = await transporter.sendMail({
+      from: "ahmed.shafique94@gmail.com",
+      to: ["ahmed.shafique94@gmail.com"],
+      subject: "Portfolio Contact Form Submission ✔",
+      html: `   <div style="border:30px solid #07243e; padding: 5%;">
+      
+      <p
+        style="
+            font-size: 32px;
+            font-weight: bold;
+            text-align: center;
+            align-self: center;
+            width: 100%;
+          "
+      >
+        Portfolio Contact Form Submission Details
+      </p>
+
+      <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Number: ${number}</p>
+        <p>Subject: ${subject}</p>
+        <p>Message: ${text}</p>
+    
+     
+
+    </div>`,
+    });
+    console.log(info);
+    if (info.messageId) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
+}
+
+export default sendMail;
